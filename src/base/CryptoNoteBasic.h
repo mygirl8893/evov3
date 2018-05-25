e74390de45eb2ee4d23b3841a1443176b1639d57
root <root@nur1labs.nur1labs.net>
2018-05-25 21:37:17 +0900

    updates

---
+++ src/base/CryptoNoteBasic.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <boost/utility/value_init.hpp>
+#include <CryptoNote.h>
+
+namespace CryptoNote {
+  const Crypto::Hash NULL_HASH = boost::value_initialized<Crypto::Hash>();
+  const Crypto::PublicKey NULL_PUBLIC_KEY = boost::value_initialized<Crypto::PublicKey>();
+  const Crypto::SecretKey NULL_SECRET_KEY = boost::value_initialized<Crypto::SecretKey>();
+
+  KeyPair generateKeyPair();
+}
