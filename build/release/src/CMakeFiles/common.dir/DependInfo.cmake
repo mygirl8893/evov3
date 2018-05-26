eef823ae8ab43f4ad046f83ee55f167768f7c004
root <root@nur1labs.nur1labs.net>
2018-05-26 22:50:11 +0900

    update algo

---
+++ build/release/src/CMakeFiles/common.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/coins/CryptonoteEvoV3-CLI/src/common/Base58.cpp" "/root/coins/CryptonoteEvoV3-CLI/build/release/src/CMakeFiles/common.dir/common/Base58.cpp.o"
+  "/root/coins/CryptonoteEvoV3-CLI/src/common/BlockingQueue.cpp" "/root/coins/CryptonoteEvoV3-CLI/build/release/src/CMakeFiles/common.dir/common/BlockingQueue.cpp.o"
+  "/root/coins/CryptonoteEvoV3-CLI/src/common/CommandLine.cpp" "/root/coins/CryptonoteEvoV3-CLI/build/release/src/CMakeFiles/common.dir/common/CommandLine.cpp.o"
+  "/root/coins/CryptonoteEvoV3-CLI/src/common/ConsoleHandler.cpp" "/root/coins/CryptonoteEvoV3-CLI/build/release/src/CMakeFiles/common.dir/common/ConsoleHandler.cpp.o"
+  "/root/coins/CryptonoteEvoV3-CLI/src/common/ConsoleTools.cpp" "/root/coins/CryptonoteEvoV3-CLI/build/release/src/CMakeFiles/common.dir/common/ConsoleTools.cpp.o"
+  "/root/coins/CryptonoteEvoV3-CLI/src/common/JsonValue.cpp" "/root/coins/CryptonoteEvoV3-CLI/build/release/src/CMakeFiles/common.dir/common/JsonValue.cpp.o"
+  "/root/coins/CryptonoteEvoV3-CLI/src/common/Math.cpp" "/root/coins/CryptonoteEvoV3-CLI/build/release/src/CMakeFiles/common.dir/common/Math.cpp.o"
+  "/root/coins/CryptonoteEvoV3-CLI/src/common/MemoryInputStream.cpp" "/root/coins/CryptonoteEvoV3-CLI/build/release/src/CMakeFiles/common.dir/common/MemoryInputStream.cpp.o"
+  "/root/coins/CryptonoteEvoV3-CLI/src/common/PathTools.cpp" "/root/coins/CryptonoteEvoV3-CLI/build/release/src/CMakeFiles/common.dir/common/PathTools.cpp.o"
+  "/root/coins/CryptonoteEvoV3-CLI/src/common/ScopeExit.cpp" "/root/coins/CryptonoteEvoV3-CLI/build/release/src/CMakeFiles/common.dir/common/ScopeExit.cpp.o"
+  "/root/coins/CryptonoteEvoV3-CLI/src/common/SignalHandler.cpp" "/root/coins/CryptonoteEvoV3-CLI/build/release/src/CMakeFiles/common.dir/common/SignalHandler.cpp.o"
+  "/root/coins/CryptonoteEvoV3-CLI/src/common/StdInputStream.cpp" "/root/coins/CryptonoteEvoV3-CLI/build/release/src/CMakeFiles/common.dir/common/StdInputStream.cpp.o"
+  "/root/coins/CryptonoteEvoV3-CLI/src/common/StdOutputStream.cpp" "/root/coins/CryptonoteEvoV3-CLI/build/release/src/CMakeFiles/common.dir/common/StdOutputStream.cpp.o"
+  "/root/coins/CryptonoteEvoV3-CLI/src/common/StreamTools.cpp" "/root/coins/CryptonoteEvoV3-CLI/build/release/src/CMakeFiles/common.dir/common/StreamTools.cpp.o"
+  "/root/coins/CryptonoteEvoV3-CLI/src/common/StringInputStream.cpp" "/root/coins/CryptonoteEvoV3-CLI/build/release/src/CMakeFiles/common.dir/common/StringInputStream.cpp.o"
+  "/root/coins/CryptonoteEvoV3-CLI/src/common/StringOutputStream.cpp" "/root/coins/CryptonoteEvoV3-CLI/build/release/src/CMakeFiles/common.dir/common/StringOutputStream.cpp.o"
+  "/root/coins/CryptonoteEvoV3-CLI/src/common/StringTools.cpp" "/root/coins/CryptonoteEvoV3-CLI/build/release/src/CMakeFiles/common.dir/common/StringTools.cpp.o"
+  "/root/coins/CryptonoteEvoV3-CLI/src/common/StringView.cpp" "/root/coins/CryptonoteEvoV3-CLI/build/release/src/CMakeFiles/common.dir/common/StringView.cpp.o"
+  "/root/coins/CryptonoteEvoV3-CLI/src/common/Util.cpp" "/root/coins/CryptonoteEvoV3-CLI/build/release/src/CMakeFiles/common.dir/common/Util.cpp.o"
+  "/root/coins/CryptonoteEvoV3-CLI/src/common/VectorOutputStream.cpp" "/root/coins/CryptonoteEvoV3-CLI/build/release/src/CMakeFiles/common.dir/common/VectorOutputStream.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "STATICLIB"
+  "UPNP_DYNAMIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../modules"
+  "version"
+  "../../src/Platform/Linux"
+  "../../src/Platform/Posix"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
